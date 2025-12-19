0206ed29b1dfdd786b96ca2e867dd3b790341904
veleztegra-create <veleztegra@gmail.com>
2025-12-19 10:58:15 -0600

    Create pantone-db.js

---
+++ js/config/pantone-db.js
@@ -0,0 +1,36 @@
+const PANTONE_DB = {
+    'UNI WHITE': '#FFFFFF',
+    'WHITE': '#FFFFFF',
+    'BLACK': '#000000',
+    'UNI RED': '#C8102E',
+    'UNI BLUE': '#003A70',
+    'UNI GREEN': '#008D62',
+    'UNI GOLD': '#FFB612',
+    'UNI SILVER': '#A5ACAF',
+    'UNI NAVY': '#0B162A',
+    'COLLEGE NAVY': '#1C2841',
+    'MARINE': '#003A70',
+    'ITALY BLUE': '#0033A0',
+    'GYM BLUE': '#005EB8',
+    'SPORT TEAL': '#008E97',
+    'ACTION GREEN': '#008D62',
+    'SEQUOIA': '#1D2624',
+    'UNIVERSITY RED': '#C8102E',
+    'TEAM RED': '#D50A0A',
+    'UNI ORANGE': '#FB4F14',
+    'TEAM ORANGE': '#FB4F14',
+    'TEAM GOLD': '#FFB612',
+    'COLOR RUSH GOLD': '#FFB612',
+    'MEDIUM SILVER': '#A7A8AA',
+    'WOLF GREY': '#9B9B9B',
+    'PEWTER GREY': '#97999B',
+    'DARK STEEL GREY': '#53565A'
+};
+
+const CLIENT_LOGOS = {
+    'NIKE': 'https://raw.githubusercontent.com/veleztegra-create/costos/refs/heads/main/Nike-Logotipo-PNG-Photo.png',
+    'FANATICS': 'https://raw.githubusercontent.com/veleztegra-create/costos/refs/heads/main/Fanatics_company_logo.svg.png',
+    'ADIDAS': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1280px-Adidas_Logo.svg.png',
+    'PUMA': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Puma_Logo.svg/1280px-Puma_Logo.svg.png',
+    'UNDER ARMOUR': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Under_armour_logo.svg/1280px-Under_armour_logo.svg.png'
+};
