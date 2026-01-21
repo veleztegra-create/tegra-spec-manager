8fdbac86e68d3ec72e5ae406588d1a6256ecf043
veleztegra-create <veleztegra@gmail.com>
2026-01-21 14:28:02 -0600

    Create dashboard.template.js

---
+++ templates/dashboard.template.js
@@ -0,0 +1,11 @@
+export const DashboardTemplate = {
+    render: () => `
+    <div class="dashboard-view">
+        <div class="welcome-banner">
+            <h2>Bienvenido al Gestor de Specs</h2>
+            <p>Seleccione una opción para comenzar</p>
+        </div>
+        <div id="dashboard-stats" class="stats-grid">
+            </div>
+    </div>`
+};
