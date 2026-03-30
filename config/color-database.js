ef46dd312846b6c88db877fb3c9b388eaa0fe0db
veleztegra-create <veleztegra@gmail.com>
2026-03-29 20:33:15 -0600

    Update color-database.js

--- config/color-database.js
+++ config/color-database.js
@@ -34,6 +34,751 @@ const colorDatabase = [
       recommendedInk: 'plastisol',
       contrastText: 'white'
     }
+  },
+  // ==================== NFL FANATICS COLORS ====================
+  {
+    name: '872 C',
+    hex: '#846F4D',
+    rgb: { r: 132, g: 111, b: 77 },
+    luminance: 113.4,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PERFLEX NATURAL - JAG RIV 15A',
+    hex: '#F3E7B1',
+    rgb: { r: 243, g: 231, b: 177 },
+    luminance: 228.43,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PEARFLEX PMS 7536 C',
+    hex: '#A79F89',
+    rgb: { r: 167, g: 159, b: 137 },
+    luminance: 158.88,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PEARFLEX BLUSTERY 45W',
+    hex: '#006676',
+    rgb: { r: 0, g: 102, b: 118 },
+    luminance: 73.33,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'TEAM MAROON 69X',
+    hex: '#792438',
+    rgb: { r: 121, g: 36, b: 56 },
+    luminance: 63.7,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'TEAM GOLD 79W',
+    hex: '#CFBA94',
+    rgb: { r: 207, g: 186, b: 148 },
+    luminance: 187.95,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'NAVY 41S',
+    hex: '#282F41',
+    rgb: { r: 40, g: 47, b: 65 },
+    luminance: 46.96,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'TEAM ORANGE 89L',
+    hex: '#E03E28',
+    rgb: { r: 224, g: 62, b: 40 },
+    luminance: 107.93,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'WHITE10A',
+    hex: '#AFAEAE',
+    rgb: { r: 175, g: 174, b: 174 },
+    luminance: 174.3,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'TEAM CRIMSON 69W',
+    hex: '#61232B',
+    rgb: { r: 97, g: 35, b: 43 },
+    luminance: 54.45,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'TEAM RED 67P',
+    hex: '#76202D',
+    rgb: { r: 118, g: 32, b: 45 },
+    luminance: 59.2,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'UNI GOLD 761',
+    hex: '#FAA818',
+    rgb: { r: 250, g: 168, b: 24 },
+    luminance: 176.1,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'GYM RED 6DL',
+    hex: '#AC162B',
+    rgb: { r: 172, g: 22, b: 43 },
+    luminance: 69.24,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'RUSH BLUE 4EW',
+    hex: '#243C76',
+    rgb: { r: 36, g: 60, b: 118 },
+    luminance: 59.44,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'COLLEGE NAVY 41S',
+    hex: '#2C3444',
+    rgb: { r: 44, g: 52, b: 68 },
+    luminance: 51.43,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'ACTION GREEN 3HN',
+    hex: '#8BC541',
+    rgb: { r: 139, g: 197, b: 65 },
+    luminance: 164.61,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'WOLF GREY 01V',
+    hex: '#A2A6AD',
+    rgb: { r: 162, g: 166, b: 173 },
+    luminance: 165.6,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PMS 430',
+    hex: '#7B888D',
+    rgb: { r: 123, g: 136, b: 141 },
+    luminance: 132.68,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'HYPER ROYAL 4NP',
+    hex: '#203182',
+    rgb: { r: 32, g: 49, b: 130 },
+    luminance: 53.15,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'UNIVERSITY GOLD 761',
+    hex: '#EEA221',
+    rgb: { r: 238, g: 162, b: 33 },
+    luminance: 170.02,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'MIDWEST GOLD 771',
+    hex: '#F9E300',
+    rgb: { r: 249, g: 227, b: 0 },
+    luminance: 207.7,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'LIGHT BONE 07G',
+    hex: '#E8E6DC',
+    rgb: { r: 232, g: 230, b: 220 },
+    luminance: 229.46,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'GYM BLUE 4LB',
+    hex: '#1D3C69',
+    rgb: { r: 29, g: 60, b: 105 },
+    luminance: 55.86,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'DK STEEL GREY 01P',
+    hex: '#767876',
+    rgb: { r: 118, g: 120, b: 118 },
+    luminance: 119.17,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: '877 C',
+    hex: '#8A9091',
+    rgb: { r: 138, g: 144, b: 145 },
+    luminance: 142.32,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'BLACK 00A',
+    hex: '#221F1F',
+    rgb: { r: 34, g: 31, b: 31 },
+    luminance: 31.9,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'UNIVERSITY RED 65N',
+    hex: '#A91D31',
+    rgb: { r: 169, g: 29, b: 49 },
+    luminance: 73.14,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'OLD ROYAL 4DA',
+    hex: '#243D7F',
+    rgb: { r: 36, g: 61, b: 127 },
+    luminance: 61.05,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'PMS 429 C',
+    hex: '#A2A9AD',
+    rgb: { r: 162, g: 169, b: 173 },
+    luminance: 167.36,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'SPORT TEAL 3JD',
+    hex: '#385758',
+    rgb: { r: 56, g: 87, b: 88 },
+    luminance: 77.84,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'MEDIUM SILVER 09D',
+    hex: '#A3A6A2',
+    rgb: { r: 163, g: 166, b: 162 },
+    luminance: 164.65,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'BLOQUER',
+    hex: '#C1C1C2',
+    rgb: { r: 193, g: 193, b: 194 },
+    luminance: 193.11,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'SPORT GREEN 3PC',
+    hex: '#264A35',
+    rgb: { r: 38, g: 74, b: 53 },
+    luminance: 60.84,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'PURPLE DYNASTY 56W',
+    hex: '#25183E',
+    rgb: { r: 37, g: 24, b: 62 },
+    luminance: 32.22,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'PALE IVORY 10L',
+    hex: '#FDF9E6',
+    rgb: { r: 253, g: 249, b: 230 },
+    luminance: 248.03,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'NATURAL 15A',
+    hex: '#F5EDD9',
+    rgb: { r: 245, g: 237, b: 217 },
+    luminance: 237.11,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PMS 872C',
+    hex: '#836F4C',
+    rgb: { r: 131, g: 111, b: 76 },
+    luminance: 112.99,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'TURBO GREEN 3GT',
+    hex: '#00968F',
+    rgb: { r: 0, g: 150, b: 143 },
+    luminance: 104.35,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'BRILLANT ORANGE 89N',
+    hex: '#F04F22',
+    rgb: { r: 240, g: 79, b: 34 },
+    luminance: 122.01,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'MARINE GREEN 46L',
+    hex: '#23556D',
+    rgb: { r: 35, g: 85, b: 109 },
+    luminance: 72.79,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'GREY VARIO',
+    hex: '#DFDEDE',
+    rgb: { r: 223, g: 222, b: 222 },
+    luminance: 222.3,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'FIELD SILVER 07R',
+    hex: '#989DA2',
+    rgb: { r: 152, g: 157, b: 162 },
+    luminance: 156.07,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'ITALY BLUE 48Y',
+    hex: '#0077BB',
+    rgb: { r: 0, g: 119, b: 187 },
+    luminance: 91.17,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'FIR 33E',
+    hex: '#0B503A',
+    rgb: { r: 11, g: 80, b: 58 },
+    luminance: 56.86,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'PITCH BLUE 4FA',
+    hex: '#26262D',
+    rgb: { r: 38, g: 38, b: 45 },
+    luminance: 38.8,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'BLUE SPARK 45A',
+    hex: '#3063A9',
+    rgb: { r: 48, g: 99, b: 169 },
+    luminance: 91.73,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PANTONE 451',
+    hex: '#9D945E',
+    rgb: { r: 157, g: 148, b: 94 },
+    luminance: 144.53,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PANTONE 7532',
+    hex: '#665849',
+    rgb: { r: 102, g: 88, b: 73 },
+    luminance: 90.48,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PANTONE 7535',
+    hex: '#AEAA9F',
+    rgb: { r: 174, g: 170, b: 159 },
+    luminance: 169.94,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PANTONE 7497',
+    hex: '#787460',
+    rgb: { r: 120, g: 116, b: 96 },
+    luminance: 114.92,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'SEAL BROWN 2DI',
+    hex: '#392E2B',
+    rgb: { r: 57, g: 46, b: 43 },
+    luminance: 48.95,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'UNI ORANGE 89M',
+    hex: '#E04326',
+    rgb: { r: 224, g: 67, b: 38 },
+    luminance: 110.64,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'MARINE 41L',
+    hex: '#272D3B',
+    rgb: { r: 39, g: 45, b: 59 },
+    luminance: 44.8,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'NEPTUNE BLUE',
+    hex: '#2178B3',
+    rgb: { r: 33, g: 120, b: 179 },
+    luminance: 100.71,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'PEWTER GREY 09T',
+    hex: '#A29E9D',
+    rgb: { r: 162, g: 158, b: 157 },
+    luminance: 159.08,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'UNI RED 65N',
+    hex: '#D2212F',
+    rgb: { r: 210, g: 33, b: 47 },
+    luminance: 87.52,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'NEW ORCHID 52M',
+    hex: '#3F3461',
+    rgb: { r: 63, g: 52, b: 97 },
+    luminance: 60.42,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'NEW ORCHID CHAMELEON',
+    hex: '#7E7391',
+    rgb: { r: 126, g: 115, b: 145 },
+    luminance: 121.71,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'YELLOW OOA',
+    hex: '#FFCD00',
+    rgb: { r: 255, g: 205, b: 0 },
+    luminance: 196.58,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'low',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'OCHRE 33B',
+    hex: '#A97728',
+    rgb: { r: 169, g: 119, b: 40 },
+    luminance: 124.94,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
+  },
+  {
+    name: 'DEEP PEWTER 2DV',
+    hex: '#423D3B',
+    rgb: { r: 66, g: 61, b: 59 },
+    luminance: 62.27,
+    toneCategory: 'dark',
+    properties: {
+      opacity: 'high',
+      recommendedInk: 'plastisol',
+      contrastText: 'white'
+    }
+  },
+  {
+    name: 'UNIVERSITY ORANGE 89M',
+    hex: '#EC5D25',
+    rgb: { r: 236, g: 93, b: 37 },
+    luminance: 129.37,
+    toneCategory: 'light',
+    properties: {
+      opacity: 'medium',
+      recommendedInk: 'waterbased',
+      contrastText: 'black'
+    }
   }
 ];
 
