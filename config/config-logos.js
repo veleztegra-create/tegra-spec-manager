a13357f6f53c0ac424eaa747f332b0093f03e719
veleztegra-create <veleztegra@gmail.com>
2026-04-14 16:06:32 -0600

    Update logo URLs for NIKE and FANATICS

--- config/config-logos.js
+++ config/config-logos.js
@@ -1,10 +1,10 @@
 
 window.LogoConfig = {
   TEGRA: 'https://raw.githubusercontent.com/veleztegra-create/costos/main/tegra%20logo.png',
-  NIKE: 'https://raw.githubusercontent.com/veleztegra-create/costos/refs/heads/main/Nike-Logotipo-PNG-Photo.png',
-  FANATICS: 'https://raw.githubusercontent.com/veleztegra-create/costos/refs/heads/main/Fanatics_company_logo.svg.png',
+  NIKE: 'https://raw.githubusercontent.com/veleztegra-create/tegra-spec-manager/refs/heads/cLabelformulas/logos/Nike-Logotipo-PNG-Photo.png',
+  FANATICS: 'https://raw.githubusercontent.com/veleztegra-create/tegra-spec-manager/refs/heads/cLabelformulas/logos/Fanatics_company_logo.svg.png',
   ADIDAS: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1280px-Adidas_Logo.svg.png',
-  PUMA: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Puma_Logo.svg/1280px-Puma_Logo.svg.png',
+  NCAA: 'https://raw.githubusercontent.com/veleztegra-create/tegra-spec-manager/refs/heads/cLabelformulas/logos/ncaa.png',
   UNDER_ARMOUR: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Under_armour_logo.svg/1280px-Under_armour_logo.svg.png',
-  GEAR_FOR_SPORT: 'https://raw.githubusercontent.com/veleztegra-create/costos/refs/heads/main/SVG.png'
+  GEAR_FOR_SPORT: 'https://raw.githubusercontent.com/veleztegra-create/tegra-spec-manager/3b139209201961263030a71c0660d333601149f1/logos/GFS.svg'
 };
